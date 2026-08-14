2ad86883f99af304c00b957ccefcc335195fd57a
yaliagasca <40399388+yaliagasca@users.noreply.github.com>
2026-08-14 01:24:54 -0600

    Map team portraits to Section C profiles

---
+++ rsh_microsite/team-photos.js
@@ -0,0 +1,7 @@
+window.TEAM_PHOTO_SPRITE='team-photos/team-portraits-sprite.jpg';
+window.TEAM_PHOTOS={
+  darin:[0,0], amy:[1,0], tim:[2,0], peter:[3,0],
+  nate:[0,1], april:[1,1], benjamin:[2,1], justin:[3,1],
+  dan:[0,2], maddie:[1,2], jessica:[2,2], nofil:[3,2],
+  rock:[0,3], andrea:[1,3]
+};
